ccdb1c9f9937b629685db70e630ffe504afd57d9
Kyle Reinholdtsen <kyle@reinholdtsen.com>
2025-04-02 05:16:09 -0700

    Add styling and social media links

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import './App.css'
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <header>
         <h1>Kyle Reinholdtsen</h1>
         <h2>Software Engineer</h2>
       </header>
-      <hr></hr>
       <article>
-        <section className="section">
+        <hr></hr>
+        <section>
           <h3>About</h3>
           <p>
             Proficient full-stack developer with 2 years of experience in web programming, object-oriented design, distributed systems, and machine learning. Thrives in collaborative environments and works effectively in teams to deliver high-quality software.
           </p>
         </section>
-        <section class="section">
+        <section>
           <h3>Skills</h3>
           <h4>Languages</h4>
           <ul>
@@ -26,17 +26,25 @@ function App() {
             <li>React.js, Node.js, Unity, PyTorch, Linux, Git.</li>
           </ul>
         </section>
-        <section class="section">
+        <section>
           <h3>Education</h3>
-          <h4>B.S. Computer Science & Engineering, University of Washington, Seattle</h4>
+          <div className='section-heading'>
+            <h4>B.S. Computer Science & Engineering</h4>
+            <span>06/2021 - 12/2023</span>
+          </div>
+          <p><i>University of Washington, Seattle</i></p>
           <ul>
-            <li>Classes: Distributed Systems, Operating Systems, Algorithms, Compilers, Machine Learning, Artificial Intelligence, Software Engineering, Computer Vision, Programming Languages, Systems Programming, Database Systems, Interaction Programming.</li>
-            <li>GPA: 3.88 (Cum Laude, Dean’s List).</li>
+            <li><b>Classes: </b>Distributed Systems, Operating Systems, Algorithms, Compilers, Machine Learning, Artificial Intelligence, Software Engineering, Computer Vision, Programming Languages, Systems Programming, Database Systems, Interaction Programming.</li>
+            <li><b>GPA: </b>3.88 (Cum Laude, Dean’s List).</li>
           </ul>
         </section>
-        <section class="section">
+        <section>
           <h3>Experience</h3>
-          <h4>Software Engineer, Husky Robotics, University of Washington, Seattle</h4>
+          <div className='section-heading'>
+            <h4>Software Engineer, Husky Robotics</h4>
+            <span>01/2022 - 03/2023</span>
+          </div>
+          <p><i>University of Washington, Seattle</i></p>
           <ul>
             <li>
               Developed the mission control website for operating the team’s rover using JavaScript, React.js, and Redux.
@@ -54,13 +62,13 @@ function App() {
             </li>
           </ul>
         </section>
-        <section class="section">
+        <section>
           <h3>Projects</h3>
-          <h4>MuseShare <a href="https://github.com/KyleR56/MuseShare">(GitHub)</a></h4>
+          <h4>MuseShare <a href='https://github.com/KyleR56/MuseShare'>(GitHub)</a></h4>
           <ul>
             <li>Built an online music sharing website for discovering and interacting with indie artists.</li>
             <li>Designed and developed the user interface using React.js, ensuring optimal performance across both
-            desktop and mobile devices</li>
+            desktop and mobile devices.</li>
             <li>Used Firebase for database management and user authentication.</li>
           </ul>
           <h4>MCQuest</h4>
@@ -70,7 +78,26 @@ function App() {
             <li>Developed server-side game logic using Java such as physics calculations, item and skill behavior, and data persistence.</li>
           </ul>
         </section>
+        <hr></hr>
       </article>
+      <footer>
+        <div className='socials'>
+          <a href='mailto:kyle@reinholdtsen.com'>
+            <i className='fas fa-envelope'></i> kyle@reinholdtsen.com
+          </a>
+          <span className='separator'>•</span>
+          <a href='https://www.linkedin.com/in/kyle-reinholdtsen-306849223/'>
+            <i className='fab fa-linkedin'></i> Kyle Reinholdtsen
+          </a>
+          <span className='separator'>•</span>
+          <a href='https://github.com/kyler56'>
+            <i className='fab fa-github'></i> kyler56
+          </a>
+        </div>
+        <a href='assets/resume.pdf' download='Kyle_Reinholdtsen_Resume.pdf' className='download-button'>
+          Download Resume <i className='fas fa-download'></i>
+        </a>
+      </footer>
     </div>
   )
 }
